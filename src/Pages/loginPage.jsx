d8440dec4e2b102e55be819574b070a29ca647e0
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-05-03 00:42:06 +0530

    update 09

--- src/Pages/loginPage.jsx
+++ src/Pages/loginPage.jsx
@@ -65,8 +65,8 @@ export default function LoginPage() {
 
       <div className="w-[50%] h-full bg-primary flex items-center justify-center">
         <div className='w-[77%] h-[96%] backdrop-blur-lg rounded-lg shadow-2xl flex flex-col items-center justify-center'>
-            <div className="w-[80%] flex flex-col">
-                <div className="text-secondary text-[24px] font-bold font-[Inter] text-left flex items-center mb-[25px]">
+            <div className="w-[80%] flex flex-col pt-2">
+                <div className="text-secondary text-[24px] font-bold font-[Inter] text-left flex items-center mb-[10px]">
                   <img src="/public/Logo.png" className="w-[50px] h-[35px] mr-1 object-fill" />
                   VetCloud
                 </div>
