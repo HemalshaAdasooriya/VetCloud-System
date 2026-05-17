463c412e25bfc5e167d849f40b2e95c72118ec01
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-05-17 16:14:56 +0530

    update 17

---
+++ Backend/models/User.js
@@ -0,0 +1,47 @@
+const db = require("../config/db");
+
+const creteUser = (userData, callback) => {
+    const sql = `INSERT INTO users
+    (
+        email,
+        password,
+        firstName,
+        lastName,
+        role,
+        isEmailVerified,
+        contact_No,
+        address,
+        is_Active,
+        image
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    db.query(sql, [
+        userData.email,
+        userData.password,
+        userData.firstName,
+        userData.lastName,
+        userData.role || "Farmer/PetOwner",
+        userData.isEmailVerified || 0,
+        userData.contact_No || null,
+        userData.address || null,
+        userData.is_Active || 0,
+        userData.image || "/default.jpg"
+    ], callback);
+};
+
+const getAllUsers = (callback) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, callback);
+};
+
+const getUserByEmail = (email, callback) => {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    db.query(sql, [email], callback);
+};
+
+module.exports = {
+    creteUser,
+    getAllUsers,
+    getUserByEmail
+};
