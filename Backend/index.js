1d9bda971bb12546e731e78d6d8b7e5073f2b5b6
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-05-17 13:54:53 +0530

    update 16💀

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const db = require("./config/db");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+});
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
