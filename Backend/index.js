cfa72ef17131ef1f7cf64c92a389c3a8949491ba
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-05-18 23:59:09 +0530

    update 20

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import userRouter from "./routes/userRouter.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
