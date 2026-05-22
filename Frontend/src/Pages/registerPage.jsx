e54cae1e83579ccc846cee5237748989f45cf190
HemalshaAdasooriya <hemalsha2022uni@gmail.com>
2026-05-22 11:57:38 +0530

    update 25

--- Frontend/src/Pages/registerPage.jsx
+++ Frontend/src/Pages/registerPage.jsx
@@ -2,17 +2,18 @@ import { Briefcase, Camera, CheckCircle2, Circle, DollarSign, Eye, EyeOff, Heart
 import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 import { TbStethoscope } from "react-icons/tb";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-import FacebookLogin from '@greatsumini/react-facebook-login';
+import FacebookLogin, { FacebookLoginClient } from '@greatsumini/react-facebook-login';
+import CustomGoogleButton from "../layouts/CustomGoogleButton";
 
 
 
 
 export default function RegisterPage() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [role, setRole] = useState('user');
     const [fullName, setFullName] = useState('');
@@ -44,9 +45,9 @@ export default function RegisterPage() {
     };
     const strength = getPasswordStrength();
 
-    // ==========================================
+
     // GOOGLE HANDLER
-    // ==========================================
+
     const handleGoogleSuccess = async (credentialResponse) => {
         setIsLoading(true);
         const backendRole = role === 'user' ? "Farmer/PetOwner" : "Veterinary Doctor";
@@ -72,9 +73,9 @@ export default function RegisterPage() {
         }
     };
 
-    // ==========================================
+    
     // FACEBOOK HANDLER
-    // ==========================================
+   
     const handleFacebookResponse = async (response) => {
         setIsLoading(true);
         const backendRole = role === 'user' ? "Farmer/PetOwner" : "Veterinary Doctor";
@@ -93,16 +94,16 @@ export default function RegisterPage() {
             } else {
                 setSubmitMessage({ text: data.message || "Facebook registration failed", isError: true });
             }
-        } catch (error) {
+        } catch{
             setSubmitMessage({ text: "Server connection failed.", isError: true });
         } finally {
             setIsLoading(false);
         }
     };
 
-    // ==========================================
+    
     // STANDARD REGISTRATION HANDLER
-    // ==========================================
+
     const handleSubmit = async (e) => {
         e.preventDefault(); 
 
@@ -202,21 +203,15 @@ export default function RegisterPage() {
                         <div className="space-y-4">
                             {/* GOOGLE BUTTON */}
                             <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-                                <div className="w-full flex justify-center border-2 border-slate-200 rounded-xl hover:border-slate-300 hover:bg-slate-50 bg-white shadow-sm hover:shadow-md transition-all duration-150 ease-in-out active:scale-95 active:shadow-inner active:bg-slate-100 active:border-slate-300 focus-within:ring-2 focus-within:ring-slate-400 focus-within:ring-offset-2 overflow-hidden py-1 cursor-pointer">
-                                    <GoogleLogin
-                                        onSuccess={handleGoogleSuccess}
-                                        onError={() => setSubmitMessage({ text: "Google Authentication failed.", isError: true })}
-                                        useOneTap 
-                                        theme="outline" 
-                                        size="large" 
-                                        text="continue_with" 
-                                        shape="rectangular"
-                                    />
-                                </div>
+                                <CustomGoogleButton 
+                                    onSuccess={handleGoogleSuccess}
+                                    onError={() => setSubmitMessage({ text: "Google Authentication failed.", isError: true })}
+                                    isLoading={isLoading}
+                                />
                             </GoogleOAuthProvider>
 
                             {/* FACEBOOK BUTTON */}
-                            <FacebookLogin
+                            <FacebookLogin.default
                                 appId={import.meta.env.VITE_FACEBOOK_APP_ID}
                                 onSuccess={handleFacebookResponse}
                                 onFail={() => setSubmitMessage({ text: "Facebook Login Failed", isError: true })}
@@ -225,7 +220,7 @@ export default function RegisterPage() {
                                         type="button"
                                         onClick={onClick}
                                         disabled={isLoading}
-                                        className="w-full flex items-center justify-center gap-3 px-6 py-[10px] bg-[#1877F2] border-2 border-[#1877F2] rounded-xl hover:bg-[#166FE5] transition-all shadow-sm"
+                                        className="w-full flex items-center justify-center gap-3 px-6 py-[10px] bg-[#1877F2] border-2 border-[#1877F2] rounded-xl hover:bg-[#166FE5] hover:shadow-md transition-all duration-150 ease-in-out shadow-sm active:shadow-inner active:scale-95 active:shadow-inner active:bg-[#1877F2] active:border-[#1877F2] focus-within:ring-[#1877F2] focus-within:ring-offset-2 overflow-hidden py-1 cursor-pointer"
                                     >
                                         <svg className="w-5 h-5" fill="white" viewBox="0 0 24 24">
                                             <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
