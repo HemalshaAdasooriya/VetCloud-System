ca35d11fbecae1cff82dbfe0c3b19c719a43d15b
Isuri Hasanjali <isurihasanjali2023@gmail.com>
2026-05-24 12:02:02 +0530

    My Animals page
    create my animal page

---
+++ Frontend/src/Pages/myAnimalsPage.jsx
@@ -0,0 +1,26 @@
+import { useState, useMemo } from "react";
+import { 
+  Search, 
+  SlidersHorizontal, 
+  Plus, 
+  FileText, 
+  Pencil, 
+  Trash2, 
+  MoreVertical, 
+  Bell, 
+  LayoutDashboard, 
+  Bird, 
+  Calendar, 
+  MessageSquare, 
+  Settings, 
+  User, 
+  LogOut,
+  X,
+  PlusCircle,
+  Activity,
+  UserCheck,
+  CheckCircle2,
+  AlertTriangle,
+  ChevronDown
+} from "lucide-react";
+import toast from "react-hot-toast";
